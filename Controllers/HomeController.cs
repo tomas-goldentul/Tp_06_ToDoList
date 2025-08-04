dcd61d7359229a176adfc3e5d3c8847b99a17b81
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-08-04 08:18:52 -0300

    Primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tp_06.Models;
+
+namespace tp_06.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
